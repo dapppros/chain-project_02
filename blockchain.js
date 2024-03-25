8dce279204484b396c8c75e9cb74b1ced4991a7f
zblockdoc <iabouzeid@zblockdoc.com>
2024-03-25 02:35:06 -0400

    Chain Validatiom

--- blockchain.js
+++ blockchain.js
@@ -6,9 +6,24 @@ class Blockchain {
   }
 
   addBlock(data) {
-    const block = Block.mineBlock(this.chain[this.chain.length-1], data);
+    const block = Block.mineBlock(this.chain[this.chain.length - 1], data);
     this.chain.push(block);
     return block;
+  }
+  
+  isValidChain(chain) {
+    if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) return false;
+    
+    for (let i = 1; i < chain.length; i++) {
+      const block = chain[i];
+      const lastBlock = chain[i - 1];
+
+      
+      if (block.lastHash !== lastBlock.hash || block.hash !== Block.hash(block.timestamp, block.lastHash, block.data)) {
+        return false;
+      }
+    }
+    return true;
   }
 }
 
